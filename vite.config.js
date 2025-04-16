d6a067794156e3d32a6a6bb98d08a599e5e22d09
Miensie <koffimiensie@gmail.com>
2025-04-16 22:01:16 +0000

    Premier commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Remplace "ton-nom-utilisateur" et "nom-du-repo"
+// https://vite.dev/config/
 export default defineConfig({
   base: '/Sangbe-Groupe-Exchange/',
   plugins: [react()],
-})
-
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+});
